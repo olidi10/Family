b0709547129deba66d94623ce823924cc280ff8b
sanja <sanja@LAPTOP-PDD355CV>
2017-02-06 20:54:58 -0600

    Add project files.

---
+++ Family/Family/FamilyPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Family
+{
+     public class FamilyPage : ContentPage
+    {
+        public FamilyPage()
+        {
+            Content = new Label()
+
+
+            {
+                Text = "Greetings, Meet my family Members\n\nFather's name\n\nMicheal\n\nBrother's name\n\nPaul!",
+                 HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.Center,
+                 FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                FontAttributes =FontAttributes.Bold,
+                BackgroundColor = Color.Yellow,
+                TextColor = Color.Blue
+               
+                
+            };
+            
+        }
+    }
+}
