ab76e32ab36db35b32164e552dc2a3b8edc01d30
sanja <sanja@LAPTOP-PDD355CV>
2017-02-09 19:53:06 -0600

    udated project

--- Family/Family/FamilyPage.cs
+++ Family/Family/FamilyPage.cs
@@ -7,7 +7,7 @@ using Xamarin.Forms;
 
 namespace Family
 {
-     public class FamilyPage : ContentPage
+    public class FamilyPage : ContentPage
     {
         public FamilyPage()
         {
@@ -15,17 +15,93 @@ namespace Family
 
 
             {
-                Text = "Greetings, Meet my family Members\n\nFather's name\n\nMicheal\n\nBrother's name\n\nPaul\n\n Mother's Name Sannah!",
-                 HorizontalTextAlignment = TextAlignment.Center,
+                FormattedText = new FormattedString                {
+                    Spans = {
+                         new Span
+                        {
+                           Text = "\nPlease Welcome to my Mobile Application." +
+                           "I will Like you to meet some important members of My Family."+
+                           "First,I will like them to introduce themselves and tell you some of thier\n\n"+
+                           "  'HOBBIES'",
+                            FontAttributes = FontAttributes.Bold | FontAttributes.Italic,
+                            ForegroundColor = Color.Black,
+                           
+                            FontSize =20,
+                           
+                            
+
+                        },
+                        new Span
+                        {
+                           Text = "\nGrandFather\n",
+                           ForegroundColor = Color.Navy,
+                            FontSize = 30,
+                        },
+                        new Span
+                        {
+                           Text = "GrandMother\n",
+                            FontAttributes = FontAttributes.Bold,
+                             ForegroundColor = Color.Maroon,
+                            FontSize = 30,
+                        },
+                        new Span
+                        {
+                           Text = "Father\n",
+                            FontAttributes = FontAttributes.Bold,
+                             ForegroundColor = Color.Green,
+                            FontSize = 30,
+                        },
+                        new Span
+                        {
+                           Text = "Mother\n",
+                            FontAttributes = FontAttributes.Bold,
+                             ForegroundColor = Color.Purple,
+                            FontSize = 30,
+                        },
+                        new Span
+                        {
+                           Text = "Brother\n",
+                            FontAttributes = FontAttributes.Italic,
+                             ForegroundColor = Color.Maroon,
+                            FontSize = 25,
+                        },
+                        new Span
+                        {
+                           Text = "Sister\n",
+                            FontAttributes = FontAttributes.Italic,
+                             ForegroundColor = Color.Olive,
+                            FontSize = 20,
+                           
+                        },
+                        new Span
+                        {
+                           Text = "Uncle\n",
+                            FontAttributes = FontAttributes.Italic,
+                             ForegroundColor = Color.Yellow,
+                            FontSize = 26,
+                        },new Span
+                        {
+                           Text = "Aunt\n",
+                            FontAttributes = FontAttributes.None,
+                            ForegroundColor = Color.Purple,
+                            FontSize = 20,
+                        },
+                    }
+                    },
+              
+                HorizontalTextAlignment = TextAlignment.Center,
                 VerticalTextAlignment = TextAlignment.Center,
-                 FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
-                FontAttributes =FontAttributes.Bold,
-                BackgroundColor = Color.Yellow,
-                TextColor = Color.Blue
-               
-                
+               // FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                FontAttributes = FontAttributes.Bold,
+                BackgroundColor = Color.Gray,
+                VerticalOptions = LayoutOptions.Center,
             };
-            
+
+
         }
+
+
     }
-}
+          
+            
+ };
